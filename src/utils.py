9686e3f99aa47b29f22e7b61a006a2c4375a902d
Hridesh14 <hridesh9012@gmail.com>
2025-12-28 21:51:58 +0530

    hi

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 from sklearn.metrics import r2_score
 
 from src.exception_handeling import CustomException
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -22,22 +23,29 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,y_train,x_test,y_test, models):
+def evaluate_model(x_train,y_train,x_test,y_test, models,params):
     try:
         report ={}
 
         for i in range (len(list(models))):
             model = list(models.values())[i]
+            model_name= list(models.keys())[i]
+            para = params[model_name]
 
-            model.fit(x_train, y_train)
+
+            gs = GridSearchCV(model,param_grid=para,cv=5)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
-
+                     
             train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test ,y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score       
+            report[list(models.keys())[i]] = test_model_score 
+            report[model_name] = test_model_score      
         return report
     
     except Exception as e:
